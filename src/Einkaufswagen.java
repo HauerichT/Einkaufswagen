553910668fc611fd1dd86d7ff4374365b1caf05d
timohaverich <timo.haverich@fh-bielefeld.de>
2022-11-11 13:20:23 +0100

    Initial Commit

--- src/Einkaufswagen.java
+++ src/Einkaufswagen.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 
 public class Einkaufswagen {
 
+    // erzeugt neue ArrayList für den Einkaufswagen
     private ArrayList<String> cart = new ArrayList<>();
 
+    // fügt Items zur cart hinzu, wenn weniger als 5 Items im Einkaufswagen sind
     public void addToCart(String item) {
         if (cart.size() < 5 ) {
             cart.add(item);
@@ -13,14 +15,17 @@ public class Einkaufswagen {
         }
     }
 
+    // entfernt das übergebene item aus dem Einkaufswagen
     public void removeFromCart(String item) {
         cart.remove(item);
     }
 
+    // gibt den Inhalt des Einkaufswagens aus
     public void showContent() {
         System.out.println("Inhalt Einkaufswagen: " + cart);
     }
 
+    // gibt die größe des Einkaufwagens aus
     public void size() {
         System.out.println(cart.size() + " Gegenstände im Einkaufswagen.");
     }
--- src/EinkaufswagenTest.java
+++ src/EinkaufswagenTest.java
@@ -1,7 +1,9 @@
 public class EinkaufswagenTest {
     public static void main(String[] args) {
 
+        // Beispiel bzw. Test aus der Aufgabe inkl. Erweiterung zur Überprüfung der Grenzfälle
         Einkaufswagen cart = new Einkaufswagen();
+        // Einkaufswagen zu beginn leer
         cart.size();
 
         cart.addToCart("Birne");
@@ -9,15 +11,17 @@ public class EinkaufswagenTest {
         cart.addToCart("Paprika");
         cart.addToCart("Salz");
         cart.addToCart("Pfeffer");
-        cart.addToCart("Öl");
 
+        // Einkaufswagen voll → Öl wird nicht hinzugefügt
+        cart.addToCart("Öl");
         cart.showContent();
 
         cart.removeFromCart("Pfeffer");
+
+        // Öl kann hinzugefügt werden, weil Pfeffer entfernt wurde und Einkaufswagen somit nicht mehr voll ist
         cart.addToCart("Öl");
 
         cart.showContent();
-
         cart.size();
 
     }
